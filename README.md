0b1935598eef452e04e4ba7d72dea1b57b7a00ec
Joel Torstensson <oed3933@gmail.com>
2017-03-28 21:44:53 +0200

    Added a cli interface

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # seedsplit
 Seedsplit lets you split a mnemonic seed into a selected number of shards which are also encoded as mnemonics. n-of-m shards can then be combined in order to get the initial mnemonic seed. This is accomplished by using Shamirs Secret Sharing.
 
-# Tests
+## Example usage
+To split the mnemonic seed:
+```
+$ seedsplit split -t 2 -s 3
+```
+You will now be prompted for a seed mnemonic.
+
+To combine mnemonics to get a seed:
+```
+$ seedsplit combine -t 2
+```
+You will be prompted for t number of mnemonics.
+
+## Tests
 To run tests:
 ```
 $ npm test
---
+++ bin/cli.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+const seedsplit = require('../lib/seedsplit')
+const program = require('commander')
+const prompt = require('prompt')
+const version = require('../package.json').version
+
+prompt.message = ''
+const prompts = {
+  seed: {
+    name: 'seed',
+    description: 'Enter seed mnemonic',
+    hidden: true,
+    required: true
+  },
+  mnemonic: {
+    name: 'mnemonic',
+    description: 'Enter shard mnemonic',
+    required: true
+  }
+}
+
+function split({ threshold, shards }) {
+  if (!threshold || !shards) {
+    console.log('Please specify threshold and shards')
+    return
+  }
+  handlePrompt(prompts.seed, result => {
+    let shardMnemonics = seedsplit.split(result.seed, shards, threshold)
+    console.log(shardMnemonics.join('\n'))
+  })
+}
+
+function combine({ threshold }) {
+  if (!threshold) {
+    console.log('Please specify threshold')
+    return
+  }
+  getMnemonics([], threshold, mnemonics => {
+    let seedMnemonic = seedsplit.combine(mnemonics)
+    console.log(seedMnemonic)
+  })
+}
+
+function getMnemonics(mnemonics, threshold, callback) {
+  if (threshold === 0) {
+    callback(mnemonics)
+  } else {
+    handlePrompt(prompts.mnemonic, result => {
+      mnemonics.push(result.mnemonic)
+      getMnemonics(mnemonics, --threshold, callback)
+    })
+  }
+}
+
+function handlePrompt(question, callback) {
+  try {
+    prompt.get(question, (err, result) => {
+      if (err) throw new Error(err)
+      callback(result)
+    })
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+program
+  .version(version)
+
+program
+  .command('split')
+  .description('Split a seed mnemonic into multiple mnemonics')
+  .option('-t, --threshold <n>', 'number of shards needed to recovery seed', parseInt)
+  .option('-s, --shards <m>', 'number of shards to create', parseInt)
+  .action(opts => split(opts))
+
+program
+  .command('combine')
+  .description('Combine multiple mnemonics into a seed mnemonic')
+  .option('-t, --threshold <n>', 'number of shards needed to recovery seed', parseInt)
+  .action(opts => combine(opts))
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
+
